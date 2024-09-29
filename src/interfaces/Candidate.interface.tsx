d78a3ae70d3331641f7aa0a0215a63197c7472cc
[mollyhoward] <mollyhoward319@gmail.com>
2024-09-29 10:52:36 -0500

    add interface

---
+++ src/interfaces/Candidate.interface.tsx
@@ -0,0 +1,13 @@
+// TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    login: string;
+    name: string;
+    location: string;
+    avatar_url: string;
+    email: string;
+    html_url: string;
+    company: string;
+}
+  
+  export default Candidate;
+  
