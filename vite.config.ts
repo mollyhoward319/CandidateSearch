44bfe522f52757e5046c6e649a40c5a88018f861
[mollyhoward] <mollyhoward319@gmail.com>
2024-09-29 12:12:20 -0500

    edit vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: './environment',
   plugins: [react()],
-  preview: {
-    host: '0.0.0.0',
-    port: 10000,
-  }
 });
