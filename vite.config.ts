d57d29e8ec87137818f2a9846395e0a8e2d34490
[mollyhoward] <mollyhoward319@gmail.com>
2024-10-05 20:13:45 -0500

    added port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './environment',
+  envDir: './',
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  }
 });
